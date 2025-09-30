02e7f39bf2cbb65127e7cc6ac71be7e15934aa04
rajatsharma-php <rajat-sharma@cssoftsolutions.com>
2025-09-30 20:41:27 +0530

    fixed done

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-_zk!%q7y@jxq5k*o+z7z3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
